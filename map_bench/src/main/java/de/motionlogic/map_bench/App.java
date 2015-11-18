340d546a855a4a00e0c936be419f66a754ece39e
Angelos Kapsimanis <angelos.kapsimanis@gmail.com>
2015-11-18 13:41:09 +0100

    Clean maven project

---
+++ map_bench/src/main/java/de/motionlogic/map_bench/App.java
@@ -0,0 +1,13 @@
+package de.motionlogic.map_bench;
+
+/**
+ * The actual benchmark
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
